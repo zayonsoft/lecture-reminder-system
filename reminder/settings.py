34697a3e5a55f6c4fb2331db12ed5b33b4dd4d0d
TONY-AKINLOSOTU Favour <favourlosotu@gmail.com>
2025-02-27 16:04:20 +0100

    Added Whitenoise

--- reminder/settings.py
+++ reminder/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'staticfiles/'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR/ "static"),
