d7e3cefdd40cbf453de382eb4818a8bc04c516ea
TONY-AKINLOSOTU Favour <favourlosotu@gmail.com>
2025-02-28 10:47:30 +0100

    Adjusted Template DIR

--- reminder/settings.py
+++ reminder/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'reminder.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['template'],
+        'DIRS': [BASE_DIR/'template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
